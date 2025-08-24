d6155fe8c912e48e99554db08518ba9ac567bf27
Ibrahim Kilani <ibrahim-kelani@hotmail.com>
2025-08-24 23:26:52 +0300

    Update l.cpp

--- Solutions/l.cpp
+++ Solutions/l.cpp
@@ -20,28 +20,25 @@ ll mod = 1000000007;
 
 
 void solve() {
-   int n;
-   cin >> n;
-   ll sum = 0;
-   for(int i = 0; i < n; i++) {
-    ll x;
-    cin >> x;
-    sum += x % mod;
+   string s;
+   cin >> s;
+
+   int a[2] = {0};
+
+   for(auto c: s) {
+    if(c == 'A') a[0]++;
+    else a[1]++;
    }
 
-
-   if(sqrt(sum) == int(sqrt(sum))) {
-    yes
-   } else {no}
+   if(a[1] > a[0]) cout << "B\n";
+   else cout << "A\n";
 }
 
 
-
- int main()
-    {
+ int main() {
     ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
     int t=1;
     cin >> t;
     while(t--)
-    solve();
-    }
+       solve();
+}
