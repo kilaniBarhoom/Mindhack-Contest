6940ddf62090e1ec09abefc0e1efaaee86eed1c9
Ibrahim Kilani <ibrahim-kelani@hotmail.com>
2025-08-25 12:22:03 +0300

    Remove LST macro for I/O synchronization

--- Solutions/h.cpp
+++ Solutions/h.cpp
@@ -6,10 +6,6 @@ typedef vector<int> vi;
 typedef vector<long long> vll;
 #define sortv(v) sort(v.begin(), v.end())
 #define sortrev(v) sort(v.begin(), v.end(), greater<int>())
-#define LST                           \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(0);                       \
-    cout.tie(0);
 #define pb push_back
 #define all(v) v.begin(), v.end()
 #define f1(i, n) for (int i = 0; i < n; i++)
